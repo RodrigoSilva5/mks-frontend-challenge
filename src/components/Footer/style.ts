03d13499f43a23364105c733387ffa19993c814b
Rodrigo Carvalho <rodrigo.software42@gmail.com>
2024-04-29 21:42:56 -0300

    ft:footer

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,15 @@
+"use client"
+
+import styled from "styled-components"
+
+export const ContainerFooter = styled.div`
+    width: 100%;
+    height: 34px;
+    background-color: #EEEEEE;
+    font-family: "Montserrat", sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
